14b757ec95c43e6262270ff525f329fb9ea1ab48
Ryan <ryjen.pears@gmail.com>
2024-02-13 20:07:54 +0100

    added dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,63 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import getPage from "./api/content";
 
+interface Duration {
+  milliseconds: number;
+  readable: string;
+}
+
+interface People {
+  directors: string[];
+  participants: string[];
+}
+
+interface Production {
+  country: string[];
+  year: number;
+}
+
+interface Product {
+  duration: Duration;
+  flags: string[];
+  genres: string[];
+  guid: string;
+  parentalRating: string;
+  people: People;
+  production: Production;
+  path: string;
+  title: string;
+  type: string;
+  imdb?: {
+    id: string;
+    rating: string;
+  };
+}
+
+interface Template {
+  href: string;
+  type: string;
+}
+
+interface Block {
+  type: string;
+  templates: Template[];
+  products: Product[];
+}
+
+interface PageData {
+  type: string;
+  blocks: Block[];
+}
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState({});
+  const [data, setData] = useState<PageData | null>(null);
 
   useEffect(() => {
     // Loads the data
     getPage()
       .then((resp) => {
         if (resp) {
-          setData(resp);
+          setData(resp as PageData);
           setIsLoading(false);
         }
       })
@@ -19,6 +66,8 @@ const App = () => {
         console.log("Something went wrong: " + error);
       });
   }, []);
+
+  console.log(data?.blocks[0].products);
 
   if (isLoading) {
     return (
